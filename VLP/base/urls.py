459f3fb5f67dc18ddca1aa6ea6c4529e23506a2f
emadmp <57809517+emadmp@users.noreply.github.com>
2021-02-28 21:54:40 +0330

    Delete urls.py

--- VLP/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path ,include
-from base import views as Bview
- 
-urlpatterns = [
-    path('' , Bview.mainView.as_view() , name = 'main'),
-    path('bycountry/<int:cu_id>/<int:page>' , Bview.bycountryView.as_view() , name = 'bycountry',)
-
-]
